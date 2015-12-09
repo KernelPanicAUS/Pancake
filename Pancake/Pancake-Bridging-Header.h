55871ca973dddb60611a2d04976929580a1beceb
Rudy Rosciglione <rosciglione@me.com>
2015-12-10 00:46:29 +0100

    Try to include Spotify API
    It is a big fail at the time.

---
+++ Pancake/Pancake-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Pancake-Bridging-Header.h
+//  Pancake
+//
+//  Created by Rudy Rosciglione on 10/12/15.
+//  Copyright © 2015 Rudy Rosciglione. All rights reserved.
+//
+
+#ifndef Pancake_Bridging_Header_h
+#define Pancake_Bridging_Header_h
+
+
+#endif /* Pancake_Bridging_Header_h */
