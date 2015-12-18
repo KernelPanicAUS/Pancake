48761b4a758be52c2517635845bbab342bfc9cb4
Rudy Rosciglione <rosciglione@me.com>
2015-12-18 14:17:29 +0100

    Spotify login Almost ready
    Indeed with Brian Advent tutorial video, i learned to implement Spotify
    authentification.

--- Pancake/Pancake-Bridging-Header.h
+++ Pancake/Pancake-Bridging-Header.h
@@ -2,12 +2,8 @@
 //  Pancake-Bridging-Header.h
 //  Pancake
 //
-//  Created by Rudy Rosciglione on 10/12/15.
+//  Created by Rudy Rosciglione on 17/12/15.
 //  Copyright © 2015 Rudy Rosciglione. All rights reserved.
 //
 
-#ifndef Pancake_Bridging_Header_h
-#define Pancake_Bridging_Header_h
-
-
-#endif /* Pancake_Bridging_Header_h */
+#import <Spotify/Spotify.h>
--- Pancake/ViewController.swift
+++ Pancake/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    let kClientID = "e644c0618991483d831ad1e34d037f7b"
+    let kCallbackURL = "pancake-app://callback/"
+    let kTokenSwapURL = "http://localhost:1234/swap"
+    let kTokenRefreshServiceURL = "http://localhost:1234/refresh"
+    
+    @IBOutlet weak var loginButton: DesignableButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Do any additional setup after loading the view, typically from a nib
     }
-
+    
+    @IBAction func loginWithSpotify(sender: AnyObject) {
+        let auth = SPTAuth.defaultInstance()
+        
+        let loginURL = auth.loginURL(kClientID, declaredRedirectURL: NSURL(string: kCallbackURL), scopes: [SPTAuthStreamingScope]!)
+        
+        UIApplication.sharedApplication().openURL(loginURL)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
