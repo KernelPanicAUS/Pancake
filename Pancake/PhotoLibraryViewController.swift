989854a4f32e44ee93cf2a83681ff345ccead9e1
Angel Vazquez <islandlens@gmail.com>
2016-01-26 23:26:29 -0400

    UICollectionView Adjustments.
    UICollectionView is now ready to go :)

--- Pancake/PhotoLibraryViewController.swift
+++ Pancake/PhotoLibraryViewController.swift
@@ -14,6 +14,9 @@ class PhotoLibraryViewController: UIViewController, UICollectionViewDelegate, UI
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // For positioning UICollectionView directly below Custom Nav Bar View
+        self.automaticallyAdjustsScrollViewInsets = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,17 +29,32 @@ class PhotoLibraryViewController: UIViewController, UICollectionViewDelegate, UI
         self.navigationController?.popViewControllerAnimated(true)
     }
     
+    // MARK: - UICollectionView
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 100
     }
-    
-    
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PHOTO_CELL", forIndexPath: indexPath)
         
         return cell
+    }
+    
+    // Sets the size of the cells
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize{
+        
+        // Adjusts cell size so that only 3 cells fit per row
+        let cellSize = (UIScreen.mainScreen().bounds.size.width / 3.0) - 1
+        
+        // Returns size of cell - Square
+        return CGSize(width: cellSize, height: cellSize)
+        
     }
     
     /*
