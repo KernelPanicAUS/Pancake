3257608ddfbcfcaf5bf2a8b6bb9bfa7b47d39522
Rudy Rosciglione <rosciglione@me.com>
2016-03-14 06:16:28 +0100

    Upload new master

---
+++ Pancake/Pancake-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#ifndef Pancake_Bridging_Header_h
+#define Pancake_Bridging_Header_h
+
+
+#import <Spotify/Spotify.h>
+
+#import "UIScrollView+EmptyDataSet.h"
+
+#import "MMPDeepSleepPreventer.h"
+
+#endif
